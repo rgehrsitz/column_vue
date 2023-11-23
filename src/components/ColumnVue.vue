462cbea79a3be74ae67de7a3da7b1ca95cd9dc8a
robertg <36572841+rgehrsitz@users.noreply.github.com>
2023-11-22 20:08:31 -0500

    Keyboard navigation working

--- src/components/ColumnVue.vue
+++ src/components/ColumnVue.vue
@@ -4,10 +4,12 @@
         <div class="column-vue">
             <div class="column" v-for="(column, columnIndex) in computedColumns" :key="columnIndex">
                 <ul>
-                    <li v-for="item in column" :key="item.uuid" :class="{ 'selected': isSelected(item) }"
-                        @click="handleItemClick(item, columnIndex)">
+                    <li v-for="(item, index) in column" :key="item.uuid" :data-uuid="item.uuid" tabindex="0"
+                        :class="{ 'selected': isSelected(item) }" @click="handleItemClick(item, columnIndex)"
+                        @keydown="handleKeyDown(item, index, $event)">
                         {{ item.name }}
                     </li>
+
                 </ul>
             </div>
         </div>
@@ -27,8 +29,9 @@ const props = defineProps({
 
 const emit = defineEmits(['select']);
 const selectedPath = ref([]);
+const selectedNode = ref(null);
 const isSelected = (item) => {
-    return selectedPath.value.some(selectedItem => selectedItem.uuid === item.uuid);
+    return selectedPath.value.some(pathItem => pathItem.uuid === item.uuid);
 };
 
 const computedColumns = computed(() => {
@@ -48,6 +51,8 @@ const computedColumns = computed(() => {
 });
 
 const handleItemClick = (item, level) => {
+
+    selectedNode.value = item;
     // Truncate the selectedPath to the current level
     selectedPath.value = selectedPath.value.slice(0, level);
 
@@ -61,6 +66,107 @@ const handleItemClick = (item, level) => {
 
 const handleBreadcrumbClick = (index) => {
     selectedPath.value = selectedPath.value.slice(0, index + 1);
+};
+
+const handleKeyDown = (item, index, event) => {
+    let targetElement;
+
+    switch (event.key) {
+        case 'ArrowUp':
+            targetElement = index > 0 ? event.target.previousElementSibling : null;
+            break;
+        case 'ArrowDown':
+            targetElement = event.target.nextElementSibling;
+            break;
+        case 'ArrowLeft':
+            targetElement = findCorrespondingItemInPreviousColumn(item);
+            break;
+        case 'ArrowRight':
+            targetElement = findFirstItemInNextColumn(item);
+            break;
+        default:
+            break;
+    }
+
+    if (targetElement) {
+        const uuid = targetElement.getAttribute('data-uuid');
+        const newItem = findItemByUuid(uuid, props.equipments);
+
+        if (newItem) {
+            selectedNode.value = newItem;
+
+            // Update selectedPath for keyboard navigation
+            updateSelectedPathForItem(newItem);
+
+            targetElement.focus();
+            event.preventDefault();
+        }
+    }
+};
+
+const findCorrespondingItemInPreviousColumn = (currentItem) => {
+    if (selectedPath.value.length < 2) {
+        // No previous column available
+        return null;
+    }
+
+    const parentIndex = selectedPath.value.length - 2;
+    const parentItem = selectedPath.value[parentIndex];
+    return document.querySelector(`.column:nth-child(${parentIndex + 1}) li[data-uuid="${parentItem.uuid}"]`);
+};
+
+
+const findFirstItemInNextColumn = (currentItem) => {
+    if (!currentItem.children || currentItem.children.length === 0) {
+        // No next column available
+        return null;
+    }
+
+    const nextColumnIndex = selectedPath.value.length + 1;
+    const firstChild = currentItem.children[0];
+    return document.querySelector(`.column:nth-child(${nextColumnIndex}) li[data-uuid="${firstChild.uuid}"]`);
+};
+
+
+
+const findItemByUuid = (uuid, items) => {
+    for (const item of items) {
+        if (item.uuid === uuid) {
+            return item;
+        }
+        if (item.children && item.children.length > 0) {
+            const found = findItemByUuid(uuid, item.children);
+            if (found) {
+                return found;
+            }
+        }
+    }
+    return null;
+};
+
+const updateSelectedPathForItem = (item) => {
+    // Reset selectedPath and rebuild it based on the new item
+    selectedPath.value = [];
+    let currentItem = item;
+    while (currentItem) {
+        selectedPath.value.unshift(currentItem);
+        currentItem = findParentItem(currentItem);
+    }
+};
+
+const findParentItem = (childItem, items = props.equipments, parent = null) => {
+    for (const item of items) {
+        if (item.uuid === childItem.uuid) {
+            return parent;
+        }
+        if (item.children && item.children.length > 0) {
+            const foundParent = findParentItem(childItem, item.children, item);
+            if (foundParent) {
+                return foundParent;
+            }
+        }
+    }
+    return null;
 };
 
 
